60790cc62fa7ae1b12e7a0f71f8907d575347144
kingshuksil <kingshuksil05@gmail.com>
2023-07-10 22:41:44 +0530

    update

--- aboutUs.php
+++ aboutUs.php
@@ -1,16 +1,106 @@
 <?php
 include("nav.php");
 ?>
+<div class="heading">About Us :</div>
+<div class="main-aboutus">
+
 <div class="card text-center">
   <div class="card-header">
-    Featured
+   Punctual Precision by Exemplary Developers
   </div>
   <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <h3 class="card-title"></h3>
+    <p class="card-text">At our company, we take immense pride in our ability to deliver software and websites on time, 
+crafted by our team of exceptional developers. With unwavering dedication, our experts ensure that 
+your projects are executed efficiently, meeting your requirements, and surpassing your 
+expectations. Count on us for timely and reliable solutions that drive your success</p>
+    <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
   </div>
+  <!-- <div class="card-footer text-body-secondary">
+    2 days ago
+  </div> -->
+</div>
 </div>
 
+<div class="main-aboutus">
+<div class="card text-center">
+  <div class="card-header">
+  Unparalleled Guidance from Industry Experts
+  </div>
+  <div class="card-body">
+    <h3 class="card-title"></h3>
+    <p class="card-text">Our team of expert faculty members comprises seasoned professionals with extensive real-life 
+expertise gained from esteemed government colleges and a remarkable tenure of 10 to 15 years in 
+the IT industry. Drawing upon their wealth of knowledge, our faculty members provide unparalleled 
+guidance and mentorship, guaranteeing the delivery of top-notch solutions that consistently exceed 
+client expectations.</p>
+    <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+  </div>
+  <!-- <div class="card-footer text-body-secondary">
+    2 days ago
+  </div> -->
+</div>
+</div>
+
+<div class="main-aboutus">
+<div class="card text-center">
+  <div class="card-header">
+  Unmatched Post-Sale Support for Enduring Success
+  </div>
+  <div class="card-body">
+    <h3 class="card-title"></h3>
+    <p class="card-text">Our commitment extends beyond the mere delivery of software or websites. We firmly believe in 
+providing exceptional after-sale service to our valued clients. With an unwavering focus on customer 
+satisfaction, our dedicated team offers continuous support and assistance, ensuring the seamless 
+functioning and maintenance of your software and websites. Count on us for unwavering post-sale 
+support, enabling your long-term success and peace of mind.</p>
+    <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+  </div>
+  <!-- <div class="card-footer text-body-secondary">
+    2 days ago
+  </div> -->
+</div>
+</div>
+
+<div class="main-aboutus">
+<div class="card text-center">
+  <div class="card-header">
+  Inspiring Collaboration and Empowering Excellence
+  </div>
+  <div class="card-body">
+    <h3 class="card-title"></h3>
+    <p class="card-text">At the heart of our company lies a friendly and motivated work culture that nurtures creativity and 
+fosters innovation. Our exceptional teams of developers thrive in an environment that encourages 
+seamless collaboration, bringing together their expertise and unwavering passion. Through this 
+collective effort, we deliver exceptional results that exceed expectations and empower your projects 
+to reach new heights of excellence</p>
+    <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+  </div>
+  <!-- <div class="card-footer text-body-secondary">
+    2 days ago
+  </div> -->
+</div>
+</div>
+
+<div class="main-aboutus">
+<div class="card text-center">
+  <div class="card-header">
+     Elevating Exam Readiness with Flawless Assessment
+  </div>
+  <div class="card-body">
+    <h3 class="card-title"></h3>
+    <p class="card-text">Prepare with utmost confidence using our unrivaled assessment platform for mock tests. Our 
+meticulously crafted question papers align precisely with the patterns of ICSE, ISC, CBSE, and IGCSE 
+boards, offering students an authentic exam-like experience. With our comprehensive and accurate 
+assessment, we empower students to effectively prepare, bolstering their confidence and 
+maximizing their potential for success</p>
+    <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+  </div>
+  <!-- <div class="card-footer text-body-secondary">
+    2 days ago
+  </div> -->
+</div>
+</div>
 <?php
 include("footer.php");
 ?>
