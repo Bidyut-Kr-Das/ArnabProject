13d67092f48f1e8134ee274a57722d23cb70284a
Bidyut-Kr-Das <bkdas2017.bd@gmail.com>
2023-07-10 11:39:11 +0530

    update

--- services.php
+++ services.php
@@ -2,13 +2,31 @@
 include("nav.php");
 ?>
 <div class="main--services">
-
     <div class="bg--svg">
         <svg id="visual" viewBox="0 0 900 600" width="900" height="600" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
             <path d="M0 218L15 221.2C30 224.3 60 230.7 90 238.8C120 247 150 257 180 268.8C210 280.7 240 294.3 270 284.3C300 274.3 330 240.7 360 226.8C390 213 420 219 450 214.3C480 209.7 510 194.3 540 190.8C570 187.3 600 195.7 630 214.7C660 233.7 690 263.3 720 268.7C750 274 780 255 810 257.2C840 259.3 870 282.7 885 294.3L900 306L900 0L885 0C870 0 840 0 810 0C780 0 750 0 720 0C690 0 660 0 630 0C600 0 570 0 540 0C510 0 480 0 450 0C420 0 390 0 360 0C330 0 300 0 270 0C240 0 210 0 180 0C150 0 120 0 90 0C60 0 30 0 15 0L0 0Z" fill="#3f42d1" stroke-linecap="round" stroke-linejoin="miter"></path>
         </svg>
     </div>
-    <h3 class="services--tittle">Services</h3>
+</div>
+<div class="service--body">
+
+    <h3 class="services--tittle">Our Features And Services</h3>
+    <div class="card--services">
+        <div class="card mb-3" style="overflow:hidden;">
+            <div class="row no-gutters" style="overflow:hidden;">
+                <div class="col-md-4 gif--image">
+                    <img src="images/card gifs/automation testing card.gif" class="card-img" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 <?php
 include("footer.php");
