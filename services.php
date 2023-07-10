267f7b27d8067a31eb8f824912cdfba724bdc040
Bidyut-Kr-Das <bkdas2017.bd@gmail.com>
2023-07-10 20:15:57 +0530

    update responsive service page

--- services.php
+++ services.php
@@ -3,8 +3,8 @@ include("nav.php");
 ?>
 
 <div class="bg--svg">
-    <svg id="visual" viewBox="0 0 900 600" width="900" height="600" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
-        <path d="M0 218L15 221.2C30 224.3 60 230.7 90 238.8C120 247 150 257 180 268.8C210 280.7 240 294.3 270 284.3C300 274.3 330 240.7 360 226.8C390 213 420 219 450 214.3C480 209.7 510 194.3 540 190.8C570 187.3 600 195.7 630 214.7C660 233.7 690 263.3 720 268.7C750 274 780 255 810 257.2C840 259.3 870 282.7 885 294.3L900 306L900 0L885 0C870 0 840 0 810 0C780 0 750 0 720 0C690 0 660 0 630 0C600 0 570 0 540 0C510 0 480 0 450 0C420 0 390 0 360 0C330 0 300 0 270 0C240 0 210 0 180 0C150 0 120 0 90 0C60 0 30 0 15 0L0 0Z" fill="#14213d" stroke-linecap="round" stroke-linejoin="miter"></path>
+    <svg id="visual" viewBox="0 0 960 540" width="960" height="540" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
+        <path d="M0 308L17.8 313.2C35.7 318.3 71.3 328.7 106.8 301.7C142.3 274.7 177.7 210.3 213.2 210C248.7 209.7 284.3 273.3 320 284.3C355.7 295.3 391.3 253.7 426.8 240.3C462.3 227 497.7 242 533.2 247.5C568.7 253 604.3 249 640 248.2C675.7 247.3 711.3 249.7 746.8 264C782.3 278.3 817.7 304.7 853.2 304.7C888.7 304.7 924.3 278.3 942.2 265.2L960 252L960 0L942.2 0C924.3 0 888.7 0 853.2 0C817.7 0 782.3 0 746.8 0C711.3 0 675.7 0 640 0C604.3 0 568.7 0 533.2 0C497.7 0 462.3 0 426.8 0C391.3 0 355.7 0 320 0C284.3 0 248.7 0 213.2 0C177.7 0 142.3 0 106.8 0C71.3 0 35.7 0 17.8 0L0 0Z" fill="#523ca2" stroke-linecap="round" stroke-linejoin="miter"></path>
     </svg>
 </div>
 
@@ -82,9 +82,9 @@ include("nav.php");
                 <div class="col-md-4 gif--image">
                     <img src="images/card gifs/mock test card.gif" class="card-img" alt="...">
                 </div>
-                <div class="col-md-8">
+                <div class="col-md-8 card--body--div">
                     <div class="card-body">
-                        <h5 class="card-title" style="overflow:hidden; font-size:3.5rem;"> Mock Tests for Class 9 to 12</h5>
+                        <h5 class="card-title" id="mockTest" style="overflow:hidden; "> Mock Tests for Class 9 to 12</h5>
                         <div class="twist--box"></div>
                         <p class="card-text">Revolutionary mock tests for unparalleled exam
                             preparation and academic excellence..</p>
