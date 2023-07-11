b8c3184bc2acb5b58e4f8617afa079efa65123e9
Bidyut-Kr-Das <bkdas2017.bd@gmail.com>
2023-07-11 13:49:20 +0530

    update footer

---
+++ serviceDigitalMarketting.php
@@ -0,0 +1,117 @@
+<?php
+include("nav.php");
+?>
+
+<div class="bg--svg">
+    <svg id="visual" viewBox="0 0 960 540" width="960" height="540" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
+        <path d="M0 308L17.8 313.2C35.7 318.3 71.3 328.7 106.8 301.7C142.3 274.7 177.7 210.3 213.2 210C248.7 209.7 284.3 273.3 320 284.3C355.7 295.3 391.3 253.7 426.8 240.3C462.3 227 497.7 242 533.2 247.5C568.7 253 604.3 249 640 248.2C675.7 247.3 711.3 249.7 746.8 264C782.3 278.3 817.7 304.7 853.2 304.7C888.7 304.7 924.3 278.3 942.2 265.2L960 252L960 0L942.2 0C924.3 0 888.7 0 853.2 0C817.7 0 782.3 0 746.8 0C711.3 0 675.7 0 640 0C604.3 0 568.7 0 533.2 0C497.7 0 462.3 0 426.8 0C391.3 0 355.7 0 320 0C284.3 0 248.7 0 213.2 0C177.7 0 142.3 0 106.8 0C71.3 0 35.7 0 17.8 0L0 0Z" fill="#523ca2" stroke-linecap="round" stroke-linejoin="miter"></path>
+    </svg>
+</div>
+
+<div class="service--body">
+
+    <h3 class="services--tittle">Digital Marketing</h3>
+    <h5 class="services--desc">In the realm of digital marketing, DSS excels in
+        delivering result-oriented strategies that maximize
+        your online presence and drive business growth.
+        Here's how we offer top-notch digital marketing
+        services:</h5>
+</div>
+<div class="card--services--body">
+    <div class="points--service">
+        <div class="twist--box1"></div>
+        <div class="twist--box2"></div>
+        <div class="textArea">
+            <div><strong>Holistic Approach: </strong> Our skilled digital marketing
+                team adopts a holistic approach to help you
+                achieve your business goals. We combine various
+                strategies, including search engine optimization
+                (SEO), social media marketing, content
+                marketing, email marketing, and more. This
+                comprehensive approach ensures that every
+                aspect of your online presence is optimized for
+                maximum impact.
+
+            </div>
+        </div>
+    </div>
+    <div class="points--service">
+        <div class="twist--box1"></div>
+        <div class="twist--box2"></div>
+        <div class="textArea">
+            <div><strong>Tailored Strategies: </strong> We understand that every
+                business is unique, with specific goals and target
+                audiences. Our team develops customized digital
+                marketing strategies that align with your business
+                objectives. By tailoring our approaches to your
+                needs, we ensure that your brand's message
+                reaches the right people at the right time,
+                resulting in improved engagement and
+                conversions.
+            </div>
+        </div>
+    </div>
+    <div class="points--service">
+        <div class="twist--box1"></div>
+        <div class="twist--box2"></div>
+        <div class="textArea">
+            <div><strong>Data-Driven Insights: </strong> At DSS, we leverage data-driven insights and industry trends to inform our
+                digital marketing strategies. By analyzing key
+                metrics and consumer behavior, we gain valuable
+                insights that enable us to optimize your
+                campaigns and deliver measurable results. Our
+                data-driven approach ensures that your
+                marketing efforts are efficient and effective.
+            </div>
+        </div>
+    </div>
+    <div class="points--service">
+        <div class="twist--box1"></div>
+        <div class="twist--box2"></div>
+        <div class="textArea">
+            <div><strong>Enhanced Brand Visibility: </strong> Our digital marketing
+                strategies are designed to enhance your brand
+                visibility in the digital landscape. Through SEO
+                techniques, social media engagement, compelling
+                content, and targeted advertising, we ensure that
+                your brand stands out among competitors.
+                Increased visibility translates to more website
+                traffic, higher engagement, and improved brand
+                recognition.
+            </div>
+        </div>
+    </div>
+    <div class="points--service">
+        <div class="twist--box1"></div>
+        <div class="twist--box2"></div>
+        <div class="textArea">
+            <div><strong>Quality Lead Generation: </strong> We prioritize generating
+                high-quality leads that have a higher likelihood of
+                conversion. By targeting the right audience,
+                crafting compelling content, and implementing
+                lead nurturing strategies, we help you attract and
+                engage potential customers who are genuinely
+                interested in your products or services. Our focus
+                is on driving leads that contribute to your
+                business growth.
+            </div>
+        </div>
+    </div>
+
+    <div class="service--body">
+        <h5 class="services--desc " style="margin-bottom:4rem;">At DSS, we combine expertise, data-driven insights,
+            and tailored strategies to deliver digital marketing
+            services that maximize your online presence and
+            drive tangible results. Our holistic approach,
+            customized strategies, emphasis on data-driven
+            insights, enhanced brand visibility, and quality lead
+            generation set us apart as a trusted partner for your
+            digital marketing needs.
+
+        </h5>
+    </div>
+</div>
+
+<?php
+include("footer.php");
+?>
---
+++ serviceMockTest.php
@@ -0,0 +1,84 @@
+<?php
+include("nav.php");
+?>
+
+<div class="bg--svg">
+    <svg id="visual" viewBox="0 0 960 540" width="960" height="540" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
+        <path d="M0 308L17.8 313.2C35.7 318.3 71.3 328.7 106.8 301.7C142.3 274.7 177.7 210.3 213.2 210C248.7 209.7 284.3 273.3 320 284.3C355.7 295.3 391.3 253.7 426.8 240.3C462.3 227 497.7 242 533.2 247.5C568.7 253 604.3 249 640 248.2C675.7 247.3 711.3 249.7 746.8 264C782.3 278.3 817.7 304.7 853.2 304.7C888.7 304.7 924.3 278.3 942.2 265.2L960 252L960 0L942.2 0C924.3 0 888.7 0 853.2 0C817.7 0 782.3 0 746.8 0C711.3 0 675.7 0 640 0C604.3 0 568.7 0 533.2 0C497.7 0 462.3 0 426.8 0C391.3 0 355.7 0 320 0C284.3 0 248.7 0 213.2 0C177.7 0 142.3 0 106.8 0C71.3 0 35.7 0 17.8 0L0 0Z" fill="#523ca2" stroke-linecap="round" stroke-linejoin="miter"></path>
+    </svg>
+</div>
+
+<div class="service--body">
+
+    <h3 class="services--tittle"> Mock Tests for Class 9 to 12</h3>
+    <h5 class="services--desc">Preparing for exams is easier with our
+        comprehensive mock test services at DSS. We
+        provide students in grades 9 to 12 with a platform to
+        assess their knowledge and boost their confidence
+        before their actual exams. Our mock tests are
+        available in both online and offline modes, offering
+        flexibility to accommodate different preferences. In
+        online mode, we offer mock tests in subjects such as
+        Physics, Chemistry, Mathematics, and Computer
+        Science, following the multiple-choice question
+        (MCQ) format. This online format aligns with the
+        examination patterns of ICSE, ISC, and CBSE boards.
+        In offline mode, we cover all subjects offered in the
+        respective curricula, providing students with an exam
+        experience similar to that of the ICSE, ISC, and CBSE
+        board exams. Our mock tests, regardless of the
+        mode, aim to provide students with valuable practice
+        resources and detailed performance analysis to help
+        them excel in their academic pursuits.
+
+    </h5>
+</div>
+<div class="card--services--body">
+
+
+    <a href="onlineExam.php">
+        <div class="card--services">
+            <div class="card mb-3" style="overflow:hidden;">
+                <div class="row no-gutters" style="overflow:hidden;">
+                    <div class="col-md-4 gif--image">
+                        <img src="images/card gifs/Online mode card.gif" class="card-img" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title" style="overflow:hidden;">Online examination</h5>
+                            <div class="twist--box"></div>
+                            <p class="card-text">In the realm of online examination, we take pride in
+                                our exceptional mock test offerings, catering to a
+                                diverse range of subjects.</p>
+                            <p class="card-text"><small class="text-muted">Click to Learn More</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </a>
+    <a href="offlineExam.php">
+        <div class="card--services">
+            <div class="card mb-3" style="overflow:hidden;">
+                <div class="row no-gutters" style="overflow:hidden;">
+                    <div class="col-md-4 gif--image">
+                        <img src="images/card gifs/offline mode card.gif" class="card-img" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title" style="overflow:hidden;"> Offline examination</h5>
+                            <div class="twist--box"></div>
+                            <p class="card-text">In the realm of offline mock tests, we proudly
+                                present an all-encompassing repertoire of subjects.</p>
+                            <p class="card-text"><small class="text-muted">Click to Learn More</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </a>
+
+
+    <?php
+    include("footer.php");
+    ?>
