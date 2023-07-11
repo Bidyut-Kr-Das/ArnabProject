92a776b2e5763174ff2d58e2b78c82d9648f6695
Bidyut-Kr-Das <bkdas2017.bd@gmail.com>
2023-07-11 10:54:28 +0530

    added software dev page

---
+++ serviceSoftwareDev.php
@@ -0,0 +1,104 @@
+<?php
+include("nav.php");
+?>
+
+<div class="bg--svg">
+    <svg id="visual" viewBox="0 0 960 540" width="960" height="540" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
+        <path d="M0 308L17.8 313.2C35.7 318.3 71.3 328.7 106.8 301.7C142.3 274.7 177.7 210.3 213.2 210C248.7 209.7 284.3 273.3 320 284.3C355.7 295.3 391.3 253.7 426.8 240.3C462.3 227 497.7 242 533.2 247.5C568.7 253 604.3 249 640 248.2C675.7 247.3 711.3 249.7 746.8 264C782.3 278.3 817.7 304.7 853.2 304.7C888.7 304.7 924.3 278.3 942.2 265.2L960 252L960 0L942.2 0C924.3 0 888.7 0 853.2 0C817.7 0 782.3 0 746.8 0C711.3 0 675.7 0 640 0C604.3 0 568.7 0 533.2 0C497.7 0 462.3 0 426.8 0C391.3 0 355.7 0 320 0C284.3 0 248.7 0 213.2 0C177.7 0 142.3 0 106.8 0C71.3 0 35.7 0 17.8 0L0 0Z" fill="#523ca2" stroke-linecap="round" stroke-linejoin="miter"></path>
+    </svg>
+</div>
+
+<div class="service--body">
+
+    <h3 class="services--tittle">Software Development</h3>
+    <h5 class="services--desc">In the realm of software development, DSS stands
+        out as a leading provider of exceptional services.
+        Here's how we demonstrate our expertise and
+        commitment to excellence:</h5>
+</div>
+<div class="card--services--body">
+    <div class="points--service">
+        <div class="twist--box1"></div>
+        <div class="twist--box2"></div>
+        <div class="textArea">
+            <div><strong>Tailored Solutions:</strong> We understand that every
+                business has unique requirements. Our team
+                specializes in crafting customized software
+                development services that precisely align with
+                your specific needs. We take the time to
+                comprehend your vision and objectives, ensuring
+                that the end product perfectly fits your business.</div>
+        </div>
+    </div>
+    <div class="points--service">
+        <div class="twist--box1"></div>
+        <div class="twist--box2"></div>
+        <div class="textArea">
+            <div><strong>Experienced Development Team: </strong> Our team of
+                developers is highly skilled and experienced in
+                the latest technologies and industry best
+                practices. With their expertise, they design, code,
+                and deploy robust software applications that are
+                scalable, secure, and efficient. We stay updated
+                with the ever-evolving technological landscape to
+                offer cutting-edge solutions.</div>
+        </div>
+    </div>
+    <div class="points--service">
+        <div class="twist--box1"></div>
+        <div class="twist--box2"></div>
+        <div class="textArea">
+            <div><strong>Client Collaboration: </strong> We believe in fostering a
+                collaborative relationship with our clients. Right
+                from the conceptualization phase to the final
+                implementation, we work closely with you,
+                actively involving you in the decision-making
+                process. We value your inputs and strive to
+                create software that exceeds your expectations.
+            </div>
+        </div>
+    </div>
+    <div class="points--service">
+        <div class="twist--box1"></div>
+        <div class="twist--box2"></div>
+        <div class="textArea">
+            <div><strong>Web, Mobile, and Enterprise Solutions: </strong> Whether
+                you need a web application, mobile app, or
+                enterprise software, we have you covered. Our
+                team is proficient in developing applications
+                across various platforms and technologies. We
+                ensure that your software is user-friendly,
+                intuitive, and optimized for performance.
+            </div>
+        </div>
+    </div>
+    <div class="points--service">
+        <div class="twist--box1"></div>
+        <div class="twist--box2"></div>
+        <div class="textArea">
+            <div><strong>Innovation and Operational Efficiency:</strong> Our
+                commitment to innovation drives us to explore
+                new technologies and incorporate industry best
+                practices into our software development
+                processes. By leveraging the latest
+                advancements, we aim to enhance your
+                operational efficiency, streamline workflows, and
+                boost productivity.
+            </div>
+        </div>
+    </div>
+    <div class="service--body">
+
+
+        <h5 class="services--desc " style="margin-bottom:4rem;">At DSS, we go above and beyond to deliver high-quality software solutions that propel innovation and
+            empower your business. Our tailored approach,
+            experienced team, client collaboration, diverse
+            expertise, and focus on operational efficiency set us
+            apart as a leading software development company.
+        </h5>
+    </div>
+</div>
+
+<?php
+include("footer.php");
+?>
